471b54830ec5769a903383a19d0c30134036726c
kros-mko <natakrisnu050107@gmail.com>
2025-02-15 14:17:00 +0800

    Add files via upload

---
+++ belajar python/main.py
@@ -0,0 +1,12 @@
+welcome_message = "WELCOME TO GENSHIN IMPACT"
+
+
+print ("##**        **##")
+# print ("_______@MIHOYO_______")
+print (f"!!!{welcome_message}!!!")
+print ("     CopyRight 2021    ")
+
+user_name = input("enter your name : ")
+
+print(user_name)
+
---
+++ belajar python/nata.py
@@ -0,0 +1,10 @@
+first_name= "Nata"
+last_name= "Krisnu"
+
+full_name= first_name +"  "+ last_name
+print(full_name)
+
+print("Hi! " * 3)
+
+
+first_name= Nata 
